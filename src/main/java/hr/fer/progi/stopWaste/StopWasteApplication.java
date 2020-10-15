6e3a4a06daece938b7532b9d5157050c8bfdf311
Josip Lukačević <jl51643@fer.hr>
2020-10-15 15:13:15 +0200

    Initial commit

---
+++ src/main/java/hr/fer/progi/stopWaste/StopWasteApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.stopWaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopWasteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopWasteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/progi/stopWaste/StopWasteApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.stopWaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
